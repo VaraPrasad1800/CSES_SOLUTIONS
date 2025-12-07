e964c4a2efdb843a641fd998350cc9f767797aa8
VaraPrasad1800 <varaprasad1800@gmail.com>
2025-12-08 00:18:46 +0530

    Add files via upload

---
+++ sliding_window_xor.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,k;
+    cin >> n >> k;
+    int x,a,b,c;
+    cin >> x >> a >> b >> c;
+
+    vector<long long> v;
+    v.push_back(x);
+    long long sum = x;
+    for(int i = 2;i<=k;i++){
+        long long val = (a*v.back()+b)%c;
+        sum^=val;
+        v.push_back(val);
+    }
+    long long sum_xor = sum ;
+    for(int i = k+1;i<=n;i++){
+        long long val = (a*v.back()+b)%c;
+        v.push_back(val);
+        sum = sum^v[i-(k+1)]^val;
+        sum_xor^=sum;
+    }
+
+    cout << sum_xor << endl;
+}
